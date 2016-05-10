4fdf5309a39bdae35d8f3bc8066269bbf9871276
Liran Tal <liran.tal@gmail.com>
2016-05-10 18:39:43 +0300

    ignoring node_modules/ due to added code samples which will create it when npm install runs. Also added sample code for helmet-security example

---
+++ code/helmet-security/index.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var express = require('express');
+var helmet = require('helmet');
+
+// web server port
+var _PORT = 3000;
+
+// helmet configuration
+var reqDuration = 2629746000;
+
+var app = express();
+
+app.use(helmet.hsts({
+  maxAge: reqDuration,
+  includeSubDomains: true
+}));
+app.use(helmet.frameguard({
+  action: 'sameorigin'
+}));
+app.use(helmet.csp({
+  directives: {
+    defaultSrc: ["'self'", 'https://ajax.googleapis.com'],
+    scriptSrc: ["'self'"],
+    styleSrc: ["'self'"],
+    childSrc: ["'none'"],
+    objectSrc: ["'none'"],
+    formSrc: ["'none'"]
+  }
+}));
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+
+
+app.get('/', function(req, res) {
+  console.info(res._headers);
+  res.status(200).send(res._headers);
+});
+
+app.listen(_PORT, function() {
+  console.info("App started on port: " + _PORT);
+});
