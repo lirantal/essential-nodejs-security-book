d0c2ed786ae9a5a171e05d80127ceb385c4537fe
Liran Tal <liran.tal@gmail.com>
2016-09-10 20:04:53 +0300

    code snippets for secure code guidelines using saferegex library

---
+++ code/secure-code-saferegex/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var saferegex = require('safe-regex');
+var emailRegex = /^([a-zA-Z0-9])(([\-.]|[_]+)?([a-zA-Z0-9]+))*(@){1}[a-z0-9]+[.]{1}(([a-z]{2,3})|([a-z]{2,3}[.]{1}[a-z]{2,3}))$/;
+
+console.log(saferegex(emailRegex));
